e84464e1a366ab0677d8d6a4856a11b459e31094
Cauã-PC <caualeu2016@gmail.com>
2023-03-14 15:19:56 -0300

    Cor Console

--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -1,6 +1,7 @@
 package chess;
 
 import boardgame.Board;
+import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMath {
@@ -35,7 +36,18 @@ public class ChessMath {
 	
 	private void initialSetup() 
 	{
-		placeNewPiece('b', 6, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 	
 	
