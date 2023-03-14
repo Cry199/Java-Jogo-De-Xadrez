f482ccd8b1230fc56b7ef0903641e6316c52ee6a
Cauã-PC <caualeu2016@gmail.com>
2023-03-14 10:31:16 -0300

    Primeira Class: Position

--- src/app/Prog.java
+++ src/app/Prog.java
@@ -1,10 +1,12 @@
 package app;
 
+import boardgame.Position;
+
 public class Prog {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
-
 }
