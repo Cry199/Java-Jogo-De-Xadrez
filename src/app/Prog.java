057b73912015922ea08007107f0b131f54b9e239
Cauã-PC <caualeu2016@gmail.com>
2023-03-15 15:11:40 -0300

    Peças capturadas

--- src/app/Prog.java
+++ src/app/Prog.java
@@ -1,6 +1,8 @@
 package app;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -14,13 +16,14 @@ public class Prog {
 	{
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 		
 		while (true)
 		{
 			try 
 			{
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				
 				System.out.println();
 				System.out.print("Source: ");
@@ -34,7 +37,12 @@ public class Prog {
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
-				ChessPiece capturePiece = chessMatch.performChessMove(source, target);
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if(capturedPiece != null)
+				{
+					captured.add(capturedPiece);
+				}
 			}
 			catch (ChessException e) 
 			{
